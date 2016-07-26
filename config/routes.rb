5461414194d01ea3b21a1eb955eedb1357e3fb7d
bananvyhe <loadonden@yahoo.com>
2016-07-26 19:58:17 +0600

    comments clean url routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+  resource :comments, only: [:new, :create] 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
